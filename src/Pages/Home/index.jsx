06f3a59b70d66a599a89bbdf75f9bbcee91560e2
carlosTangarife <cjt860826@gmail.com>
2024-08-07 22:33:56 -0500

    load dynamic products to be rendered dynamically

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,23 @@
+import {useState, useEffect} from "react";
 import {Layout} from '../../Components/Layout';
-import {Card} from "../../Components/Card/index.jsx";
+import {Card} from "../../Components/Card";
 
 export const Home = () => {
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data));
+    }, []);
+
     return (
         <div className="">
             <Layout>
                 Home
-                <Card/>
+                <div className="grid gap-r grid-cols-4 w-full max-w-screen-lg">
+                    {items?.map((item, index) => <Card key={index} {...item} />)}
+                </div>
             </Layout>
         </div>
     )
