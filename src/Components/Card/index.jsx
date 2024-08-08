bd2a0c99a132d0c6094a0bebbc80644e1f16264a
carlosTangarife <cjt860826@gmail.com>
2024-08-07 23:52:28 -0500

    added details to product details

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,20 +3,26 @@ import {ShoppingCartContext} from "../../Context";
 
 import { PlusIcon } from '@heroicons/react/24/solid'
 
-export const Card = ({title, price, images, category}) => {
-    const { count, setCount} = useContext(ShoppingCartContext);
+export const Card = (props) => {
+    const {title, price, images, category} = props;
+    const { count, setCount, openProductDetail, setProductToShow} = useContext(ShoppingCartContext);
 
     const addToCart = () => {
         setCount(count + 1);
     }
 
+    const showProductDetail = (productDetail) => {
+        openProductDetail();
+        setProductToShow(productDetail)
+    }
+
     return (
-        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+        <div className="bg-white cursor-pointer w-56 h-60 rounded-lg" onClick={() => showProductDetail(props)} >
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{category?.name}</span>
-                <img className="w-full h-full object-cover rounded-lg" src={images[0]} alt="headphones"/>
+                <img className="w-full h-full object-cover rounded-lg" src={images} alt="headphones"/>
                 <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1" onClick={addToCart}>
-                    <PlusIcon className="h-6 w-6 text-black" />
+                    <PlusIcon className="h-6 w-6 text-black cursor-pointer" />
                 </div>
             </figure>
             <p className="flex justify-between">
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,9 +5,20 @@ export const ShoppingCartContext = createContext();
 export const ShoppingCartProvider = ({children}) => {
     const [count, setCount] = useState(0);
 
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    const [productToShow, setProductToShow] = useState({});
+
     return (<ShoppingCartContext.Provider value={{
         count,
-        setCount
+        setCount,
+        isProductDetailOpen,
+        openProductDetail,
+        closeProductDetail,
+        productToShow,
+        setProductToShow
     }}>
         {children}
     </ShoppingCartContext.Provider>)
